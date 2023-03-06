342ab1088a4edf0ef45b951178c221ef2978e058
Daniel Beringer <danielberinger95@gmail.com>
2023-03-06 16:44:08 +0100

    Add CustomTkinter license copy

--- app/gui.py
+++ app/gui.py
@@ -1,9 +1,17 @@
+# CustomTkinter - MIT License
+# Copyright (c) 2023 Tom Schimansky
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 import textwrap
 import tkinter
 import customtkinter
 import pandas as pd
-from pandas import DataFrame
-
 import logic
 from models.Collection import CmsColl, BankColl
 from models.CmsFileHelper import CmsFileHelper
@@ -12,7 +20,6 @@ from tkinter import messagebox, filedialog, simpledialog
 from config import PROJECT_DIR
 from configparser import ConfigParser
 from config import DEF_F_EXTENSION
-from sys import stdout
 
 app_id = 'dberinger.apps.collectionsgenerator.version'  # arbitrary string
 ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(app_id)
